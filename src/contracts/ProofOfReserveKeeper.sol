585320cda644c2961c8f944004420dda97baec48
rustem-kurmaev <rustboyar@gmail.com>
2022-08-11 17:24:31 +0300

    Add and remove reserve

--- src/contracts/ProofOfReserveKeeper.sol
+++ src/contracts/ProofOfReserveKeeper.sol
@@ -1,14 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IAaveProofOfReserve } from '../interfaces/IAaveProofOfReserve.sol';
+import {IAaveProofOfReserve} from '../interfaces/IAaveProofOfReserve.sol';
+import {KeeperCompatible} from 'lib/chainlink-brownie-contracts/contracts/src/v0.8/KeeperCompatible.sol';
 
-contract ProofOfReserveKeeper is IAaveProofOfReserve {
-  function addReserve(address bridgedAsset, address reserveFeed) public {
+contract ProofOfReserveKeeper is
+  IAaveProofOfReserve,
+  KeeperCompatibleInterface
+{
+  // struct Reserve {
+  //   address asseet;
+  //   address proofOfReserveFeed;
+  // }
 
+  address[] public assets;
+  mapping(address => address) public proofOfReserves;
+
+  function addReserve(address asset, address reserveFeed) public {
+    // require risk admin access
+    // should we add asset if it already exists?
+    // should we use extra mapping ?
+    proofOfReserves[asset] = reserveFeed;
+
+    for (uint256 i = 0; i < assets.length; i++) {
+      if (assets[i] == asset) {
+        return;
+      }
+    }
+
+    assets.push(bridgedAsset);
   }
 
-  function removeReserve(address bridgedAsset) public {
-
+  function removeReserve(address asset) public {
+    // require risk admin access
+    for (uint256 i = 0; i < assets.length; i++) {
+      if (assets[i] == asset) {
+        // remove
+      }
+    }
   }
+
+  function checkUpkeep(
+    bytes calldata /* checkData */
+  )
+    external
+    view
+    override
+    returns (
+      bool upkeepNeeded,
+      bytes memory /* performData */
+    )
+  {
+    upkeepNeeded = false;
+  }
+
+  function performUpkeep(
+    bytes calldata /* performData */
+  ) external override {}
 }
