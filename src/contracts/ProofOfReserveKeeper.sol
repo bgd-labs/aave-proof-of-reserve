62dc3df7db7e57b69b910722df2e2c09fb24e029
rustem-kurmaev <rustboyar@gmail.com>
2022-08-11 11:20:53 +0300

    AddProofOfReserveKeeper contract

---
+++ src/contracts/ProofOfReserveKeeper.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IAaveProofOfReserve } from '../interfaces/IAaveProofOfReserve.sol';
+
+contract ProofOfReserveKeeper is IAaveProofOfReserve {
+  function addReserve(address bridgedAsset, address reserveFeed) public {
+
+  }
+
+  function removeReserve(address bridgedAsset) public {
+
+  }
+}
