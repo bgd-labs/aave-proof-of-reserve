b95f03f4ab9bc66f2ae372ea1b5b2d41ceab47c1
rustem-kurmaev <rustboyar@gmail.com>
2022-08-17 16:02:22 +0300

    Comments for contract

--- src/interfaces/IAaveProofOfReserve.sol
+++ src/interfaces/IAaveProofOfReserve.sol
@@ -4,19 +4,46 @@ pragma solidity ^0.8.0;
 import {IPool} from '../dependencies/IPool.sol';
 
 interface IAaveProofOfReserve {
+  /**
+   * @dev emitted when new asset and it's proof of reserve feed are enabled or disabled
+   * @param asset address of the asset
+   * @param proofOfReserveFeed address of the PoR feed
+   * @param enabled whether it was enabled or disabled
+   */
   event ProofOfReserveFeedStateChanged(
     address indexed asset,
     address indexed proofOfReserveFeed,
     bool enabled
   );
+
+  /**
+   * @dev emitted when borrowing of all assets on the market is disabled
+   * @param user address of the user who inited the action
+   */
   event EmergencyActionExecuted(address indexed user);
 
+  /**
+   * @dev add assets and corresponding proof of reserve feed to the registry.
+   * @param asset address of the asset
+   * @param proofOfReserveFeed address of the proof of reserve aggregator feed
+   */
   function enableProofOfReserveFeed(address asset, address proofOfReserveFeed)
     external;
 
+  /**
+   * @dev delete asset and proof of reserve feed from the registry.
+   * @param asset address of the asset
+   */
   function disableProofOfReserveFeed(address asset) external;
 
+  /**
+   * @dev returns if all the assets in the registry are backed.
+   */
   function areAllReservesBacked() external view returns (bool);
 
+  /**
+   * @dev disable borrowing for all the assets on the pool when at least
+   * one of the assets in the registry is not backed.
+   */
   function executeEmergencyAction(IPool pool) external;
 }
