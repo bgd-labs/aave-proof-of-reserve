cc1a23ba35e3f19ba4364766b5b6380b9443435a
rustem-kurmaev <rustboyar@gmail.com>
2022-08-29 11:07:23 +0300

    Proposal code with mock addresses

--- src/proposal/ProposalPayloadProofOfReserve.sol
+++ src/proposal/ProposalPayloadProofOfReserve.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {AaveV2Avalanche, AaveV3Avalanche} from 'aave-address-book/AaveAddressBook.sol';
+import {ILendingPoolAddressesProvider} from 'aave-address-book/AaveV2.sol';
+import {IACLManager} from 'aave-address-book/AaveV3.sol';
+import {IProofOfReserveAggregator} from '../interfaces/IProofOfReserveAggregator.sol';
+import {IProofOfReserveExecutor} from '../interfaces/IProofOfReserveExecutor.sol';
+
 /**
  * @title ProposalPayloadProofOfReserve
  * @author BGD Labs
@@ -12,6 +18,63 @@ pragma solidity ^0.8.0;
  * - V3: assign Risk admin role to ProofOfReserveExecutorV3
  * - V3: enable tokens for checking against their proof of reserfe feed
  */
+
 contract ProposalPayloadProofOfReserve {
-  function execute() external {}
+  bytes32 public constant PROOF_OF_RESERVE_ADMIN = 'PROOF_OF_RESERVE_ADMIN';
+
+  address public constant LENDING_POOL_CONFIGURATOR_IMPL = address(0);
+  address public constant PROOF_OF_RESERVE_AGGREGATOR = address(0);
+  address public constant PROOF_OF_RESERVE_EXECUTOR_V2 = address(0);
+  address public constant PROOF_OF_RESERVE_EXECUTOR_V3 = address(0);
+
+  address[] public ASSETS = [address(0), address(1)];
+  address[] public PROOF_OF_RESERVE_FEEDS = [address(10), address(11)];
+
+  function execute() external {
+    // Aggregator
+    IProofOfReserveAggregator aggregator = IProofOfReserveAggregator(
+      PROOF_OF_RESERVE_AGGREGATOR
+    );
+
+    for (uint256 i; i < ASSETS.length; i++) {
+      aggregator.enableProofOfReserveFeed(ASSETS[i], PROOF_OF_RESERVE_FEEDS[i]);
+    }
+
+    // V2
+    ILendingPoolAddressesProvider addressProvider = AaveV2Avalanche
+      .POOL_ADDRESSES_PROVIDER;
+
+    // set new implementation for pool configurator
+    addressProvider.setLendingPoolConfiguratorImpl(
+      LENDING_POOL_CONFIGURATOR_IMPL
+    );
+
+    // set executor v2 as proof of reserve admin
+    addressProvider.setAddress(
+      PROOF_OF_RESERVE_ADMIN,
+      PROOF_OF_RESERVE_EXECUTOR_V2
+    );
+    IProofOfReserveExecutor executorV2 = IProofOfReserveExecutor(
+      PROOF_OF_RESERVE_EXECUTOR_V2
+    );
+
+    // enable asset to be validated by proof of reserve
+    for (uint256 i; i < ASSETS.length; i++) {
+      executorV2.enableAsset(ASSETS[i]);
+    }
+
+    // V3
+    IACLManager aclManager = AaveV3Avalanche.ACL_MANAGER;
+
+    aclManager.addRiskAdmin(PROOF_OF_RESERVE_EXECUTOR_V3);
+
+    IProofOfReserveExecutor executorV3 = IProofOfReserveExecutor(
+      PROOF_OF_RESERVE_EXECUTOR_V3
+    );
+
+    // enable assets to be validated by proof of reserve
+    for (uint256 i; i < ASSETS.length; i++) {
+      executorV3.enableAsset(ASSETS[i]);
+    }
+  }
 }
