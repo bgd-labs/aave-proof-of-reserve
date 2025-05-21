bedb32a73100671680c7599051ec4bbb7e768461
ianflexa <ianflexa@gmail.com>
2025-05-21 14:54:58 -0400

    refactor: optimized bitmap aligned with v3 + removed unused variables

--- src/helpers/ReserveConfiguration.sol
+++ src/helpers/ReserveConfiguration.sol
@@ -4,41 +4,20 @@ pragma solidity ^0.8.0;
 import {DataTypes as DataTypesV2} from 'aave-address-book/AaveV2.sol';
 import {DataTypes as DataTypesV3} from 'aave-address-book/AaveV3.sol';
 
+/**
+ * @title ReserveConfiguration library Adapted
+ * @author BGD Labs
+ * @notice Implements the bitmap logic to handle the reserve configuration
+ * @dev Adapted version to support both V2 and V3 configurations with minimal
+ * bitmap logic required by the ProofOfReserveExecutor contract.
+ */
 library ReserveConfiguration {
-  uint256 internal constant LTV_MASK =                   0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000; // prettier-ignore
-  uint256 internal constant LIQUIDATION_THRESHOLD_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000FFFF; // prettier-ignore
-  uint256 internal constant LIQUIDATION_BONUS_MASK =     0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000FFFFFFFF; // prettier-ignore
-  uint256 internal constant DECIMALS_MASK =              0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00FFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant ACTIVE_MASK =                0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant FROZEN_MASK =                0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDFFFFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant BORROWING_MASK =             0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFFFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant STABLE_BORROWING_MASK =      0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant RESERVE_FACTOR_MASK =        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000FFFFFFFFFFFFFFFF; // prettier-ignore
-  uint256 internal constant EMODE_CATEGORY_MASK =        0xFFFFFFFFFFFFFFFFFFFF00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF; // prettier-ignore
-
-  /// @dev For the LTV, the start bit is 0 (up to 15), hence no bitshifting is needed
-  uint256 internal constant LIQUIDATION_THRESHOLD_START_BIT_POSITION = 16;
-  uint256 internal constant LIQUIDATION_BONUS_START_BIT_POSITION = 32;
-  uint256 internal constant RESERVE_DECIMALS_START_BIT_POSITION = 48;
-  uint256 internal constant IS_ACTIVE_START_BIT_POSITION = 56;
-  uint256 internal constant IS_FROZEN_START_BIT_POSITION = 57;
-  uint256 internal constant BORROWING_ENABLED_START_BIT_POSITION = 58;
-  uint256 internal constant STABLE_BORROWING_ENABLED_START_BIT_POSITION = 59;
-  uint256 internal constant IS_PAUSED_START_BIT_POSITION = 60;
-  uint256 internal constant BORROWABLE_IN_ISOLATION_START_BIT_POSITION = 61;
-  uint256 internal constant SILOED_BORROWING_START_BIT_POSITION = 62;
-  /// @dev bit 63 reserved
-  uint256 internal constant RESERVE_FACTOR_START_BIT_POSITION = 64;
-  uint256 internal constant BORROW_CAP_START_BIT_POSITION = 80;
-  uint256 internal constant SUPPLY_CAP_START_BIT_POSITION = 116;
-  uint256 internal constant LIQUIDATION_PROTOCOL_FEE_START_BIT_POSITION = 152;
-  uint256 internal constant EMODE_CATEGORY_START_BIT_POSITION = 168;
-  uint256 internal constant UNBACKED_MINT_CAP_START_BIT_POSITION = 176;
-  uint256 internal constant DEBT_CEILING_START_BIT_POSITION = 212;
+  uint256 internal constant FROZEN_MASK =                0x0000000000000000000000000000000000000000000000000200000000000000; // prettier-ignore
+  uint256 internal constant BORROWING_MASK =             0x0000000000000000000000000000000000000000000000000400000000000000; // prettier-ignore
 
   /**
    * @dev Gets the borrowing state of the reserve
-   * @param self The reserve configuration
+   * @param self The V2 reserve configuration
    * @return The borrowing state
    **/
   function getBorrowingEnabled(DataTypesV2.ReserveConfigurationMap memory self)
@@ -46,49 +25,32 @@ library ReserveConfiguration {
     pure
     returns (bool)
   {
-    return (self.data & ~BORROWING_MASK) != 0;
+    return (self.data & BORROWING_MASK) != 0;
   }
 
   /**
    * @dev Gets the frozen state of the reserve
-   * @param self The reserve configuration
+   * @param self The V2 reserve configuration
    * @return The frozen state
    **/
-  function getIsFrozen(DataTypesV2.ReserveConfigurationMap memory self)
+  function getFrozen(DataTypesV2.ReserveConfigurationMap memory self)
     internal
     pure
     returns (bool)
   {
-    return (self.data & ~FROZEN_MASK) != 0;
+    return (self.data & FROZEN_MASK) != 0;
   }
 
   /**
-   * @notice Gets the configuration parameters of the reserve from storage
-   * @param self The reserve configuration
-   * @return The state param representing ltv
-   * @return The state param representing liquidation threshold
-   * @return The state param representing liquidation bonus
-   * @return The state param representing frozen state
+   * @dev Gets the frozen state of the reserve
+   * @param self The V3 reserve configuration
+   * @return The frozen state
    **/
-  function getReserveParams(DataTypesV3.ReserveConfigurationMap memory self)
+  function getFrozen(DataTypesV3.ReserveConfigurationMap memory self)
     internal
     pure
-    returns (
-      uint256,
-      uint256,
-      uint256,
-      bool
-    )
+    returns (bool)
   {
-    uint256 dataLocal = self.data;
-
-    return (
-      dataLocal & ~LTV_MASK,
-      (dataLocal & ~LIQUIDATION_THRESHOLD_MASK) >>
-        LIQUIDATION_THRESHOLD_START_BIT_POSITION,
-      (dataLocal & ~LIQUIDATION_BONUS_MASK) >>
-        LIQUIDATION_BONUS_START_BIT_POSITION,
-      (dataLocal & ~FROZEN_MASK) != 0
-    );
+    return (self.data & FROZEN_MASK) != 0;
   }
 }
