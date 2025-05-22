6ca049d91b0db59922199515414ddda2b6e2aa17
ianflexa <ianflexa@gmail.com>
2025-05-22 10:29:36 -0400

    chore: undo the update of solc in executor contracts

--- src/contracts/ProofOfReserveExecutorV2.sol
+++ src/contracts/ProofOfReserveExecutorV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.0;
 
 import {DataTypes, ILendingPoolAddressesProvider, ILendingPool, ILendingPoolConfigurator} from 'aave-address-book/AaveV2.sol';
 import {ProofOfReserveExecutorBase} from './ProofOfReserveExecutorBase.sol';
