e594c480ed8928960ae0b9ff4aa5f22e7a16c0e1
ianflexa <ianflexa@gmail.com>
2025-05-23 15:07:45 -0400

    fix: certora ci paths and solc version

--- certora/scripts/runAggregator.sh
+++ certora/scripts/runAggregator.sh
@@ -4,5 +4,5 @@ certoraRun certora/harness/ProofOfReserveAggregatorHarness.sol \
     --loop_iter 3 \
     --solc solc8.27 \
     --cloud \
-    --packages solidity-utils/=lib/aave-helpers/lib/aave-address-book/lib/aave-v3-origin/lib/solidity-utils/src/  \
+    --packages solidity-utils=lib/aave-helpers/lib/aave-address-book/lib/aave-v3-origin/lib/solidity-utils/src @openzeppelin/contracts=lib/aave-helpers/lib/aave-address-book/lib/aave-v3-origin/lib/solidity-utils/lib/openzeppelin-contracts-upgradeable/lib/openzeppelin-contracts/contracts aave-v3-origin=lib/aave-helpers/lib/aave-address-book/lib/aave-v3-origin/src  \
     --msg "ProofOfReserveAggregator"
